52276c2a68de4b827f49b5259e21b839a87c478a
Kevin Peter Hickerson <kevinh@caltech.edu>
2011-10-05 15:02:55 -0700

    Works. Added files for a push.

---
+++ units.js
@@ -0,0 +1,54 @@
+// unit per neV: neV, Tesla, Gauss, meters, cm, Kelvin, mK
+// unit per m/s: m/s, cm/s, seconds
+var unit = {
+    "neV" : {
+        words: "has energy", 
+        exp: 1, 
+        coef: 1
+    },
+    "Tesla" : {
+        words: "is repelled by a field of",
+        exp: 1, 
+        coef: 60.3077
+    },
+    "Gauss" : {
+        words:  "is repelled by a field of",
+        exp: 1, 
+        coef: 0.00603077
+    },
+    "meters" : {
+        words:  "falls from",
+        exp: 1, 
+        coef: 102.519
+    },
+    "cm" : {
+        words:  "falls from",
+        exp: 1, 
+        coef: 1.02519
+    },
+    "Kelvin" : {
+        words: "has temperature",
+        exp: 1, 
+        coef: 86173.4
+    },
+    "mK" : {
+        words: "has temperature",
+        exp: 1, 
+        coef: 86.1734
+    },
+    "m/s" : {
+        words: "is moving at",
+        exp: 2, 
+        coef: 5.2270385
+    },
+    "cm/s" : {
+        words: "is moving at",
+        exp: 2, 
+        coef: 0.00052270385
+    },
+    "seconds": {
+        words: "reaches apogee in",
+        exp: 2, 
+        coef: 502.686
+    }
+};
